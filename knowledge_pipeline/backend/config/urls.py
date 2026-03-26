0dbe4648ff0d7cf37124d1408ac5bb3ab2efe627
P-tiphart Suebpeng <pwlnwzarediooo@gmail.com>
2026-03-26 11:39:11 +0700

    [DELETE]

--- knowledge_pipeline/backend/config/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api.views import upload_document
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('api/upload/', upload_document),
-]
--- knowledge_pipeline/backend/create_superuser.py
+++
@@ -1,8 +0,0 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-from django.contrib.auth import get_user_model
-User = get_user_model()
-su_username = os.environ.get('SUPER_USER_BACKEND', 'admin')
-if not User.objects.filter(username=su_username).exists():
-    User.objects.create_superuser(su_username, f'{su_username}@example.com', os.environ.get('SUPER_USER_BACKEND_PASSWORD', 'qwer1234'))
